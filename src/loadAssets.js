746073182427a15ad6c2305343f8b6405de4dd0d
giannatg <giannatn@uci.edu>
2024-12-17 13:15:01 -0800

    Add cat images for block 2

--- src/loadAssets.js
+++ src/loadAssets.js
@@ -1,5 +1,14 @@
 import jsPsychPreload from '@jspsych/plugin-preload';
 
+import cat1 from './assets/cat/1.jpg';
+import cat2 from './assets/cat/2.jpg';
+import cat3 from './assets/cat/3.jpg';
+import cat4 from './assets/cat/4.jpg';
+import cat5 from './assets/cat/5.jpg';
+
+// Reference these files in a new array
+const catImages = [cat1, cat2, cat3, cat4, cat5];
+
 // Create arrays of hot dog / not hot dog images
 const numFiles = 5;
 const hotDogFiles = Array.from(Array(numFiles), (_, i) => i + 1).map(
